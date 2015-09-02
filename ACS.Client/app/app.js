5d3d8b4945ccc9b97e294b198ce9484ecd36dd4b
unknown <herki77@gmail.com>
2015-09-02 21:14:26 +0100

    Added functioality to AngularJS do throw exceptions out and functionality do catch them in nunit c#

--- ACS.Client/app/app.js
+++ ACS.Client/app/app.js
@@ -2,5 +2,19 @@
     "use strict";
 
     var app = angular.module("app",
-        ["common.services", "ui.bootstrap"]);
+        ["common.services", "ui.bootstrap"])
+        .provider({
+            $exceptionHandler: function(){
+                var handler = function(exception, cause) {
+                    exception.message += ' (caused by "' + cause + '")';
+                    throw exception;
+                };
+
+                this.$get = function() {
+                    return handler;
+                };   
+            }
+        });
+
+
 }());
---
+++ ACS.StartTrekTesting/Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace ACS.StartTrekTesting
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void Testing()
+        {
+            //var options = new ChromeOptions();
+            //options.AddExtension("extension.crx");
+            var optn = new ChromeOptions();
+            var service = ChromeDriverService.CreateDefaultService();
+            service.LogPath = "chromedriver.log";
+            service.EnableVerboseLogging = true;
+            optn.AddExtension("extension.crx");
+            var driver = new ChromeDriver(service, optn);
+            //var driver = new ChromeDriver(options);
+            
+            
+
+
+            driver.Navigate().GoToUrl("http://localhost:64834/");
+
+            
+            var javascriptDriver = (IJavaScriptExecutor) driver;
+            var errors = javascriptDriver.ExecuteScript("return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : [];");
+            
+            //var writer = new StreamWriter("jsErrors.log");
+            var collection = errors as ReadOnlyCollection<object>;
+            foreach (var item in collection)
+            {
+                var errorObject = item as Dictionary<string, object>;
+                foreach (var field in errorObject)
+                {
+                    Console.WriteLine(field.Key + " - " + field.Value);
+                }
+                Console.WriteLine("-------------------");
+            }
+            //writer.Flush();
+            //writer.Close();
+
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
