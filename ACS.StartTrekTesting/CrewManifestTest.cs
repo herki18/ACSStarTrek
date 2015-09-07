722e8d0957e65185f1a862fd2c33b1ba5e8d2ff7
unknown <herki77@gmail.com>
2015-09-07 15:36:37 +0100

    Refactored code and started prototyping chrome extension do get all information inside browser console

---
+++ ACS.StartTrekTesting/CrewManifestTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ACS.StartTrekTesting
+{
+    [TestFixture]
+    public class CrewManifestTest
+    {
+        private ChromeDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var optn = new ChromeOptions();
+            var service = ChromeDriverService.CreateDefaultService();
+            service.LogPath = "chromedriver.log";
+            service.EnableVerboseLogging = true;
+            optn.AddExtension("extension.crx");
+            _driver = new ChromeDriver(service, optn);
+        }
+
+
+        [Test]
+        public void Navigation_GoDoCrewManifestPage_ShowCrewManifestPage()
+        {
+            
+            _driver.Navigate().GoToUrl("http://localhost:64834/");
+            InjectLogger(_driver);
+            GetLoggs(_driver);    
+            CollectLogsFromBrowser(_driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+
+        private void GetLoggs(IJavaScriptExecutor driver)
+        {
+            var logs = driver.ExecuteScript(@"return store");
+        }
+
+        private void InjectLogger(IJavaScriptExecutor driver)
+        {
+            driver.ExecuteScript("var store = []; var oldf = console.log; console.log = function(){ store.push(arguments); oldf.apply(console, arguments); };");
+        }
+
+        private void CollectLogsFromBrowser(IJavaScriptExecutor driver)
+        {
+            var errors = driver.ExecuteScript("return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : [];");
+
+            var collection = errors as ReadOnlyCollection<object>;
+            foreach (var item in collection)
+            {
+                var errorObject = item as Dictionary<string, object>;
+                foreach (var field in errorObject)
+                {
+                    Console.WriteLine(field.Key + " - " + field.Value);
+                }
+                Console.WriteLine("-------------------");
+            }   
+        }
+    }
+}
